efc04d398ead7e53583dd4ef817e70e1dd3c0c56
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-16 05:08:07 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,34 +1,25 @@
-# Use either if statements or boolean expressions to control the return value of a method
-  # Must move to a position within the tic-tac-toe board
-  # The position must be vacant, not currently taken by a player
-# use a "helper method" - a method called within another method - to make your code more readable
-  # Build a #valid_move? method that accepts a board and an index to check and returns true if the move is valid and false or nil if not.
-  # A valid move means that the submitted position is:
-    # Present on the game board
-    # Not already filled with a token
-def user_selection(int)
-  num = int - 1
-  if num.between?(0,8)
-    num
-  else
-    nil
-  end
-end
-
-
-def position_taken?(board, index)
-  position = user_selection(index)
-  if board[position] == " " || board[position] == "" || board[position] == nil
-    return false
-  else
-    return true
-  end
-end
-
 def valid_move?(board, index)
- if position_taken?(board, index) == false
-   return true
- else
-   false
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
   end
- end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
+end
